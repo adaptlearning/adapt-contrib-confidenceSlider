aeb3c6022309584253c8cad744ebc9df2cfaa5cd
Nils Sanderson <nils@learningpool.com>
2019-03-29 13:53:10 +0000

    update to use 2 spaces for indentation

--- js/adapt-contrib-confidenceSlider.js
+++ js/adapt-contrib-confidenceSlider.js
@@ -1,56 +1,56 @@
 define([
-    'core/js/adapt',
-    './confidenceSliderView',
-    './confidenceSliderModel'
+  'core/js/adapt',
+  './confidenceSliderView',
+  './confidenceSliderModel'
 ], function(Adapt, ConfidenceSliderView, ConfidenceSliderModel) {
 
-    Adapt.on('app:dataReady', function() {
-        // is tracking enabled?
-        if (Adapt.config.has('_spoor') && Adapt.config.get('_spoor')._isEnabled) {
-            // if spoor is handling response tracking we don't need to do anything
-            if (!Adapt.config.get('_spoor')._tracking._shouldStoreResponses) {
-                // ensure data is setup
-                Adapt.offlineStorage.get();
+  Adapt.on('app:dataReady', function() {
+    // is tracking enabled?
+    if (Adapt.config.has('_spoor') && Adapt.config.get('_spoor')._isEnabled) {
+      // if spoor is handling response tracking we don't need to do anything
+      if (!Adapt.config.get('_spoor')._tracking._shouldStoreResponses) {
+        // ensure data is setup
+        Adapt.offlineStorage.get();
 
-                _.each(Adapt.components.where({'_component': 'confidenceSlider', '_shouldStoreResponses': true}), function(confidenceSlider) {
-                    var dataItem = Adapt.offlineStorage.get(confidenceSlider.get('_id'));
+        _.each(Adapt.components.where({'_component': 'confidenceSlider', '_shouldStoreResponses': true}), function(confidenceSlider) {
+          var dataItem = Adapt.offlineStorage.get(confidenceSlider.get('_id'));
 
-                    if (!dataItem) return;
+          if (!dataItem) return;
 
-                    var numericParameters = dataItem[0];
-                    var booleanParameters = dataItem[1];
+          var numericParameters = dataItem[0];
+          var booleanParameters = dataItem[1];
 
-                    var score = numericParameters[0];
-                    var attemptsLeft = numericParameters[1] || 0;
+          var score = numericParameters[0];
+          var attemptsLeft = numericParameters[1] || 0;
 
-                    var hasUserAnswer = booleanParameters[0];
-                    var isUserAnswerArray = booleanParameters[1];
-                    var isInteractionComplete = booleanParameters[2];
-                    var isSubmitted = booleanParameters[3];
-                    var isCorrect = booleanParameters[4];
+          var hasUserAnswer = booleanParameters[0];
+          var isUserAnswerArray = booleanParameters[1];
+          var isInteractionComplete = booleanParameters[2];
+          var isSubmitted = booleanParameters[3];
+          var isCorrect = booleanParameters[4];
 
-                    confidenceSlider.set({
-                        _isComplete: true,
-                        _isInteractionComplete: isInteractionComplete,
-                        _isSubmitted: isSubmitted,
-                        _score: score,
-                        _isCorrect: isCorrect,
-                        _attemptsLeft: attemptsLeft
-                    });
+          confidenceSlider.set({
+            _isComplete: true,
+            _isInteractionComplete: isInteractionComplete,
+            _isSubmitted: isSubmitted,
+            _score: score,
+            _isCorrect: isCorrect,
+            _attemptsLeft: attemptsLeft
+          });
 
-                    if (hasUserAnswer) {
-                        var userAnswer = dataItem[2];
-                        if (!isUserAnswerArray) userAnswer = userAnswer[0];
+          if (hasUserAnswer) {
+            var userAnswer = dataItem[2];
+            if (!isUserAnswerArray) userAnswer = userAnswer[0];
 
-                        confidenceSlider.set('_userAnswer', userAnswer);
-                    }
-                });
-            }
-        }
-    });
+            confidenceSlider.set('_userAnswer', userAnswer);
+          }
+        });
+      }
+    }
+  });
 
-    return Adapt.register('confidenceSlider', {
-        view: ConfidenceSliderView,
-        model: ConfidenceSliderModel
-    });
+  return Adapt.register('confidenceSlider', {
+    view: ConfidenceSliderView,
+    model: ConfidenceSliderModel
+  });
 });
--- js/confidenceSliderView.js
+++ js/confidenceSliderView.js
@@ -1,131 +1,131 @@
 define([
-    'core/js/adapt',
-    'components/adapt-contrib-slider/js/sliderView',
+  'core/js/adapt',
+  'components/adapt-contrib-slider/js/sliderView',
 ], function(Adapt, SliderView) {
 
-    var ConfidenceSliderView = SliderView.extend({
+  var ConfidenceSliderView = SliderView.extend({
 
-    	/* override */
-        preRender:function() {
-            if (this.model.has('_linkedModel') && this.model.get('_linkedModel').get('_isSubmitted') && !this.model.get('_isSubmitted')) {
-                this.model.set('_isEnabled', true);
-            }
+    /* override */
+    preRender:function() {
+      if (this.model.has('_linkedModel') && this.model.get('_linkedModel').get('_isSubmitted') && !this.model.get('_isSubmitted')) {
+        this.model.set('_isEnabled', true);
+      }
 
-            SliderView.prototype.preRender.apply(this, arguments);
-        },
+      SliderView.prototype.preRender.apply(this, arguments);
+    },
 
-        /* override */
-        setupQuestion: function() {
-            if (this.model.get('_linkedToId')) {
-                this.listenTo(Adapt, "buttonsView:postRender", this.onButtonsRendered);
-            }
-            SliderView.prototype.setupQuestion.apply(this, arguments);
-        },
+    /* override */
+    setupQuestion: function() {
+      if (this.model.get('_linkedToId')) {
+        this.listenTo(Adapt, "buttonsView:postRender", this.onButtonsRendered);
+      }
+      SliderView.prototype.setupQuestion.apply(this, arguments);
+    },
 
-        /* override */
-        disableQuestion: function() {
-            if (this.model.get('_isReady')) this.setAllItemsEnabled(false);
-            if (this.model.has('_linkedModel')) this.$('.buttons-action').a11y_cntrl_enabled(false);
-        },
+    /* override */
+    disableQuestion: function() {
+      if (this.model.get('_isReady')) this.setAllItemsEnabled(false);
+      if (this.model.has('_linkedModel')) this.$('.buttons-action').a11y_cntrl_enabled(false);
+    },
 
-        /* override */
-        enableQuestion: function() {
-            if (this.model.get('_isReady')) this.setAllItemsEnabled(true);
-            if (this.model.has('_linkedModel')) this.$('.buttons-action').a11y_cntrl_enabled(true);
-        },
+    /* override */
+    enableQuestion: function() {
+      if (this.model.get('_isReady')) this.setAllItemsEnabled(true);
+      if (this.model.has('_linkedModel')) this.$('.buttons-action').a11y_cntrl_enabled(true);
+    },
 
-        _listenToLinkedModel: function() {
-            this.listenTo(this.model.get('_linkedModel'), {
-                'change:_selectedItem': this.onLinkedConfidenceChanged,
-                'change:_isSubmitted': this.onLinkedSubmittedChanged
-            });
-        },
+    _listenToLinkedModel: function() {
+      this.listenTo(this.model.get('_linkedModel'), {
+        'change:_selectedItem': this.onLinkedConfidenceChanged,
+        'change:_isSubmitted': this.onLinkedSubmittedChanged
+      });
+    },
 
-        _updateLinkedConfidenceIndicator: function() {
-            var lm = this.model.get('_linkedModel');
-            var linkedValue = 0;
-            var rangeslider = this.$slider.data('plugin_rangeslider');
+    _updateLinkedConfidenceIndicator: function() {
+      var lm = this.model.get('_linkedModel');
+      var linkedValue = 0;
+      var rangeslider = this.$slider.data('plugin_rangeslider');
 
-            linkedValue = lm.has('_userAnswer') ? lm.get('_userAnswer') : lm.get('_selectedItem').value;
+      linkedValue = lm.has('_userAnswer') ? lm.get('_userAnswer') : lm.get('_selectedItem').value;
 
-            if (linkedValue == this.model.get('_scaleEnd')) {
-                this.$('.linked-confidence-bar').css({width: '100%'});
-            }
-            else {
-                // follow rangeslider setPosition method
-                this.$('.linked-confidence-bar').css({width: (rangeslider.getPositionFromValue(linkedValue) + rangeslider.grabPos) + 'px'});
-            }
-        },
+      if (linkedValue == this.model.get('_scaleEnd')) {
+        this.$('.linked-confidence-bar').css({width: '100%'});
+      }
+      else {
+        // follow rangeslider setPosition method
+        this.$('.linked-confidence-bar').css({width: (rangeslider.getPositionFromValue(linkedValue) + rangeslider.grabPos) + 'px'});
+      }
+    },
 
-        onQuestionRendered: function() {
-            SliderView.prototype.onQuestionRendered.apply(this, arguments);
+    onQuestionRendered: function() {
+      SliderView.prototype.onQuestionRendered.apply(this, arguments);
 
-            if (this.model.has('_linkedModel')) {
-                this.$('.rangeslider').prepend($('<div class="linked-confidence-bar"/>'));
-                this._listenToLinkedModel();
-                if (this.model.get('_linkedModel').get('_isSubmitted')) {
-                    this.onLinkedConfidenceChanged();
-                } else {
-                    this.model.set('_isEnabled', false);
-                    this.$('.component-body-inner').html(this.model.get('disabledBody'));
-                }
-            }
-
-            if (this.model.get('_isSubmitted') && this.model.has('_userAnswer')) {
-                this.model.set({
-                    feedbackTitle: this.model.get('title'),
-                    feedbackMessage: this.model.getFeedbackString()
-                });
-            }
-        },
-
-        onScreenSizeChanged: function() {
-            SliderView.prototype.onScreenSizeChanged.apply(this, arguments);
-
-            // if linked slider on same page update it with user interaction
-            if (this.model.has('_linkedModel') && this.model.get('_linkedModel').get('_isReady')) {
-                this._updateLinkedConfidenceIndicator();
-            }
-        },
-
-        onResetClicked: function() {
-            SliderView.prototype.onResetClicked.apply(this, arguments);
-
-            this.model.reset('soft', true);
-
-            this.model.updateTracking();
-        },
-
-        onSubmitClicked: function() {
-            SliderView.prototype.onSubmitClicked.apply(this, arguments);
-
-            this.model.updateTracking();
-        },
-
-        onButtonsRendered: function(buttonsView) {
-            // necessary due to deferred ButtonsView::postRender
-            if (this.buttonsView == buttonsView) {
-                if (!this.model.get('_isEnabled')) {
-                    if (!this.model.has('_linkedModel') || !this.model.get('_linkedModel').get('_isSubmitted')) {
-                        this.$('.buttons-action').a11y_cntrl_enabled(false);
-                    }
-                }
-            }
-        },
-
-        onLinkedConfidenceChanged: function() {
-            this._updateLinkedConfidenceIndicator();
-        },
-
-        onLinkedSubmittedChanged: function(linkedModel) {
-            if (linkedModel.get('_isSubmitted')) {
-                this.model.set('_isEnabled', true);
-            }
-            else {
-                this.model.set('_isEnabled', false);
-            }
+      if (this.model.has('_linkedModel')) {
+        this.$('.rangeslider').prepend($('<div class="linked-confidence-bar"/>'));
+        this._listenToLinkedModel();
+        if (this.model.get('_linkedModel').get('_isSubmitted')) {
+          this.onLinkedConfidenceChanged();
+        } else {
+          this.model.set('_isEnabled', false);
+          this.$('.component-body-inner').html(this.model.get('disabledBody'));
         }
-    });
+      }
 
-    return ConfidenceSliderView;
+      if (this.model.get('_isSubmitted') && this.model.has('_userAnswer')) {
+        this.model.set({
+          feedbackTitle: this.model.get('title'),
+          feedbackMessage: this.model.getFeedbackString()
+        });
+      }
+    },
+
+    onScreenSizeChanged: function() {
+      SliderView.prototype.onScreenSizeChanged.apply(this, arguments);
+
+      // if linked slider on same page update it with user interaction
+      if (this.model.has('_linkedModel') && this.model.get('_linkedModel').get('_isReady')) {
+        this._updateLinkedConfidenceIndicator();
+      }
+    },
+
+    onResetClicked: function() {
+      SliderView.prototype.onResetClicked.apply(this, arguments);
+
+      this.model.reset('soft', true);
+
+      this.model.updateTracking();
+    },
+
+    onSubmitClicked: function() {
+      SliderView.prototype.onSubmitClicked.apply(this, arguments);
+
+      this.model.updateTracking();
+    },
+
+    onButtonsRendered: function(buttonsView) {
+      // necessary due to deferred ButtonsView::postRender
+      if (this.buttonsView == buttonsView) {
+        if (!this.model.get('_isEnabled')) {
+          if (!this.model.has('_linkedModel') || !this.model.get('_linkedModel').get('_isSubmitted')) {
+            this.$('.buttons-action').a11y_cntrl_enabled(false);
+          }
+        }
+      }
+    },
+
+    onLinkedConfidenceChanged: function() {
+      this._updateLinkedConfidenceIndicator();
+    },
+
+    onLinkedSubmittedChanged: function(linkedModel) {
+      if (linkedModel.get('_isSubmitted')) {
+        this.model.set('_isEnabled', true);
+      }
+      else {
+        this.model.set('_isEnabled', false);
+      }
+    }
+  });
+
+  return ConfidenceSliderView;
 });
